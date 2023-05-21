d1ff4715f8460258a28a2b22f6819510890bf947
Dmitry Kalinin <djimabebeshko@gmail.com>
2023-05-22 00:26:30 +0300

    add an entry to new scope in semantic/src/ast/block.rs

--- semantic/src/ast/block.rs
+++ semantic/src/ast/block.rs
@@ -10,11 +10,15 @@ impl Semanticize for frontend::ast::Block {
     type Item = Block;
 
     fn semanticize(self, env: &mut Environment) -> Result<Self::Item, Error> {
+        env.variables.fork();
+
         let statements = self
             .statements
             .into_iter()
             .map(|body| body.semanticize(env))
             .collect::<Result<Vec<_>, _>>()?;
+
+        env.variables.exit();
 
         Ok(Block { statements })
     }
